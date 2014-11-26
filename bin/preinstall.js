362204e6454253b51b7206a013c38b0809afdee6
Justin Hays <jhays@adc4gis.com>
2014-11-26 14:37:37 -0600

    initial commit

---
+++ bin/preinstall.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+var npm = require('npm')
+var path = require('path')
+
+var libDir = path.resolve(__dirname,'../lib/')
+var noop = function(){}
+
+npm.load(function () {
+  fs.readdirSync(libDir).forEach(function (mod) {
+    npm.prefix = path.join(libDir, mod)
+    npm.config.set('save', false)
+    npm.commands.install(noop)
+  })
+})
